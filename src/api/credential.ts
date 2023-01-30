ebdf68d6ee078297753f51ace50d00f5157db103
Kiran Pachhai <kiran.pachhai@gmail.com>
2023-01-30 17:29:59 -0500

    Prettify

--- src/api/credential.ts
+++ src/api/credential.ts
@@ -1,49 +1,57 @@
-import { IIdentifier, VerifiablePresentation } from '@veramo/core';
-import express, { NextFunction } from 'express';
-import { getAgent, setupAgent } from './../veramo/setup';
-import * as middlewares from './../middlewares';
+import { IIdentifier, VerifiablePresentation } from '@veramo/core'
+import express, { NextFunction } from 'express'
+import { getAgent, setupAgent } from './../veramo/setup'
+import * as middlewares from './../middlewares'
 
-const router = express.Router();
+const router = express.Router()
 
-type CredentialResponse = string[];
+type CredentialResponse = string[]
 
-router.use(middlewares.init);
+router.use(middlewares.init)
 router.post('/register/', async (req, res) => {
-  
-  console.log('body' + JSON.stringify(req.body));
+  console.log('body' + JSON.stringify(req.body))
 
-  let agent = await getAgent();
-  let identifier = await agent.didManagerCreate({ kms: 'local', provider: 'did:pkh', options: { network: 'eip155', chainId: "1"} });
+  let agent = await getAgent()
+  let identifier = await agent.didManagerCreate({
+    kms: 'local',
+    provider: 'did:pkh',
+    options: { network: 'eip155', chainId: '1' },
+  })
   const credential = await agent.createVerifiableCredential({
     proofFormat: 'jwt',
     credential: {
-      credentialSubject: { id: req.body.identifier, loginName: req.body.loginName },
-      type: ['Login'],
+      credentialSubject: {
+        id: req.body.identifier,
+        loginName: req.body.loginName,
+      },
+      type: ['SiteLoginCredential'],
       issuer: identifier.did,
     },
   })
-  
-  console.log(JSON.stringify(credential));
-  res.json(credential);
-});
+
+  console.log(JSON.stringify(credential))
+  res.json(credential)
+})
 
 router.post('/signIn', async (req, res) => {
-  console.log('presentation:' + JSON.stringify(req.body.presentation));
-  let agent = await getAgent();
+  console.log('presentation:' + JSON.stringify(req.body.presentation))
+  let agent = await getAgent()
 
-  let verifiablePresentation: VerifiablePresentation = req.body.presentation as VerifiablePresentation;
+  let verifiablePresentation: VerifiablePresentation = req.body
+    .presentation as VerifiablePresentation
 
-  let verified  = await agent.verifyPresentation({ presentation: verifiablePresentation});
+  let verified = await agent.verifyPresentation({
+    presentation: verifiablePresentation,
+  })
 
+  res.json(verified.verified)
+})
 
-  res.json(verified.verified);
-});
-
-export default router;
-function getAgentIdentifier() : Promise<IIdentifier>{
-  throw new Error('Function not implemented.');
+export default router
+function getAgentIdentifier(): Promise<IIdentifier> {
+  throw new Error('Function not implemented.')
 }
 
-function getUserIdentifier() : Promise<IIdentifier>{
-  throw new Error('Function not implemented.');
+function getUserIdentifier(): Promise<IIdentifier> {
+  throw new Error('Function not implemented.')
 }
